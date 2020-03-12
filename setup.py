248f7059805939d9768e79c030a3de9bc57eb658
jesopo <github@lolnerd.net>
2020-03-12 16:46:04 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("VERSION", "r") as version_file:
+    version = version_file.read().strip()
+with open("requirements.txt", "r") as requirements_file:
+    install_requires = requirements_file.read().splitlines()
+
+setuptools.setup(
+    name="ircstates",
+    version=version,
+    author="jesopo",
+    author_email="pip@jesopo.uk",
+    description="IRC client session state parsing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jesopo/ircstates",
+    packages=setuptools.find_packages(),
+    package_data={"irctokens": ["py.typed"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Communications :: Chat :: Internet Relay Chat"
+    ],
+    python_requires='>=3.6',
+    install_requires=install_requires
+)
