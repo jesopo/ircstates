3e55f39bb73348d8870d4550e98a2a44fe1f33a0
jesopo <github@lolnerd.net>
2020-03-15 23:28:04 +0000

    test nicknames being casefolded on JOIN

--- test/casemap.py
+++ test/casemap.py
@@ -1,11 +1,18 @@
 import unittest
-from ircstates.casemap import casefold
+import ircstates, irctokens
 
 class CaseMapTest(unittest.TestCase):
     def test_rfc1459(self):
-        lower = casefold("rfc1459", "ÀTEST[]~\\")
+        lower = ircstates.casefold("rfc1459", "ÀTEST[]~\\")
         self.assertEqual(lower, "Àtest{}^|")
 
     def test_ascii(self):
-        lower = casefold("ascii", "ÀTEST[]~\\")
+        lower = ircstates.casefold("ascii", "ÀTEST[]~\\")
         self.assertEqual(lower, "Àtest[]~\\")
+
+    def test_join(self):
+        server = ircstates.Server("test")
+        server.parse_tokens(irctokens.tokenise("001 Nickname"))
+        server.parse_tokens(irctokens.tokenise(":Nickname JOIN #chan"))
+        server.parse_tokens(irctokens.tokenise(":Other JOIN #chan"))
+        self.assertIn("other", server.users)
