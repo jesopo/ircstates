90def4aba529adea921389b476825ffc45f60d50
jesopo <github@lolnerd.net>
2020-04-08 21:23:02 +0100

    add WHOIS numerics

--- ircstates/numerics.py
+++ ircstates/numerics.py
@@ -7,9 +7,10 @@ def _numeric(number: str, name: str):
 
 _numeric("001", "RPL_WELCOME")
 _numeric("005", "RPL_ISUPPORT")
+_numeric("372", "RPL_MOTD")
+_numeric("375", "RPL_MOTDSTART")
 _numeric("221", "RPL_UMODEIS")
-
-_numeric("311", "RPL_WHOISUSER")
+_numeric("396", "RPL_VISIBLEHOST")
 
 _numeric("324", "RPL_CHANNELMODEIS")
 _numeric("329", "RPL_CREATIONTIME")
@@ -20,10 +21,6 @@ _numeric("352", "RPL_WHOREPLY")
 _numeric("353", "RPL_NAMREPLY")
 _numeric("366", "RPL_ENDOFNAMES")
 
-_numeric("372", "RPL_MOTD")
-_numeric("375", "RPL_MOTDSTART")
-_numeric("396", "RPL_VISIBLEHOST")
-
 _numeric("903", "RPL_SASLSUCCESS")
 _numeric("904", "ERR_SASLFAIL")
 _numeric("905", "ERR_SASLTOOLONG")
@@ -31,4 +28,11 @@ _numeric("906", "ERR_SASLABORTED")
 _numeric("907", "ERR_SASLALREADY")
 _numeric("908", "RPL_SASLMECHS")
 
-
+_numeric("311", "RPL_WHOISUSER")
+_numeric("312", "RPL_WHOISSERVER")
+_numeric("313", "RPL_WHOISOPERATOR")
+_numeric("317", "RPL_WHOISIDLE")
+_numeric("378", "RPL_WHOISHOST")
+_numeric("379", "RPL_WHOISMODES")
+_numeric("671", "RPL_WHOISSECURE")
+_numeric("318", "RPL_ENDOFWHOIS")
