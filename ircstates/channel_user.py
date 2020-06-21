7a87b7b44876cfa7ac46fcc1367d35296ca70148
jesopo <github@lolnerd.net>
2020-06-21 22:04:26 +0100

    add ChannelUser.__repr__

--- ircstates/channel_user.py
+++ ircstates/channel_user.py
@@ -10,9 +10,19 @@ class ChannelUser(object):
 
         self.modes: List[str] = []
 
+    def __repr__(self) -> str:
+        outs: List[str] = [self.channel, self.nickname]
+        if self.modes:
+            outs.append(f"+{''.join(self.modes)}")
+        return f"ChannelUser({' '.join(outs)})"
+
     @property
     def nickname(self) -> str:
         return self._nickname.normal
     @property
     def nickname_lower(self) -> str:
         return self._nickname.folded
+
+    @property
+    def channel(self) -> str:
+        return self._channel_name.normal
